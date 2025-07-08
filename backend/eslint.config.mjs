85e25ca2bfb7ecf006aef2fdb30a78f04bbac171
Martial <floquet.martial@gmail.com>
2025-07-08 21:47:08 +0200

    chore(backend): update ESlint config for Typescript support

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -2,5 +2,20 @@ import { defineConfig } from "eslint/config";
 
 
 export default defineConfig([
+  {
+    files: ["**/*.ts"],
+    plugins: { "@typescript-eslint": tseslint.plugin },
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        project: "./tsconfig.json",
+      },
+    },
+    rules: {
+    
+      "no-unused-vars": "warn",
+      "no-explicit-any": "error",
+    },
+  },
   { ignores: ["**/*.js", "**/*.cjs", "**/*.mjs"] },
 ]);
