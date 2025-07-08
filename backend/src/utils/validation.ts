5164680e7f7dd7af99724b16773ad1317208caed
Martial <floquet.martial@gmail.com>
2025-07-08 17:09:51 +0200

    docs(utils): add commentary

--- backend/src/utils/validation.ts
+++ backend/src/utils/validation.ts
@@ -1,10 +1,13 @@
 import { Compute, OperationType } from '../shared/types';
 
+// Vérifie si l'opération demandée est une des 4 autorisées donc add, substract, multiply, divide.
 
 export function isValidOperation(operation: string): operation is OperationType {
   const validOperations: OperationType[] = ['add', 'substract', 'multiply', 'divide'];
   return validOperations.includes(operation as OperationType);
 }
+
+// On vérifie l'objet  (opération + 2 nombres)
 
 export function validateCompute(compute: unknown): compute is Compute {
   if (!compute || typeof compute !== 'object' || compute === null) {
