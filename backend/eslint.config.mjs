00a1cfd51ee0cffade6eec38b50d8251eaf05bc0
Martial <floquet.martial@gmail.com>
2025-07-08 22:08:41 +0200

    fix(backend): fix ESLint config and imports for working lint

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -1,23 +1,22 @@
 import { defineConfig } from "eslint/config";
-
+import plugin from "@typescript-eslint/eslint-plugin";
+import parser from "@typescript-eslint/parser";
 
 export default defineConfig([
   {
     files: ["**/*.ts"],
-    plugins: { "@typescript-eslint": tseslint.plugin },
+    plugins: { "@typescript-eslint": plugin },
     languageOptions: {
-      parser: tseslint.parser,
+      parser,
       parserOptions: {
         project: "./tsconfig.json",
       },
     },
     rules: {
-    
       "no-unused-vars": "warn",
-      "no-explicit-any": "error",
-      "no-console": "error",
+      "@typescript-eslint/no-explicit-any": "error",
       "no-var": "error",
-      "no-magic-numbers": "warn",
+      "no-console": "warn",
     },
   },
   { ignores: ["**/*.js", "**/*.cjs", "**/*.mjs"] },
