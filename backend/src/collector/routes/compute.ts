c0e38744984b07ef38bd7f970e0007d6bb14652a
Martial <floquet.martial@gmail.com>
2025-07-09 13:12:44 +0200

    feat(collector): create Task object and store it in Redis in /compute route
    Generate a unique task id, build the Task object, and persist it in Redis for later processing.

--- backend/src/collector/routes/compute.ts
+++ backend/src/collector/routes/compute.ts
@@ -1,11 +1,21 @@
 import { Request, Response } from 'express'
 import { validateCompute } from '../../utils/validation'
+import { v4 as uuidv4 } from 'uuid';
+import { Task } from '../../shared/types';
+import redisClient from '../../shared/redis';
 
 export const compute = async (req: Request, res: Response): Promise<void> => {
     if (!validateCompute(req.body)) {
         res.status(400).json({ status: "error", message: "Invalid compute payload. 'operation' doit être une string parmi [add, substract, multiply, divide] et 'operands' doit être un tableau de deux nombres." });
         return;
     }
-   
+    const id = uuidv4();
+    const task: Task = {
+        id,
+        compute: req.body,
+        result: "",
+        status: "pending"
+    };
+    await redisClient.set(id, JSON.stringify(task));
     res.json({status: "ok"});
 }
