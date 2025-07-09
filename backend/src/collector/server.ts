8da88611aa27029e455867f61f4d9f348998af35
Martial <floquet.martial@gmail.com>
2025-07-09 11:51:28 +0200

    feat(collector): integrate compute route into Express server

--- backend/src/collector/server.ts
+++ backend/src/collector/server.ts
@@ -6,6 +6,7 @@ dotenv.config()
 import express from 'express'
 import { Request, Response } from 'express'
 import logger from '../shared/logger'
+import { compute } from './routes/compute'
 
 const app = express()
 const port = process.env.COLLECTOR_PORT
@@ -15,6 +16,9 @@ app.use(express.json())
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!')
 })
+
+
+app.post('/compute', compute)
 
 app.listen(port, () => {
   logger.info(`Collector server running on port ${port}`)
