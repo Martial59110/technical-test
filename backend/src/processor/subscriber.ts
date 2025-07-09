6a48be4be3200330c9705834ebbef0776fcbda12
Martial <floquet.martial@gmail.com>
2025-07-09 19:02:35 +0200

    feat(processor): connect to Redis in startSubscriber

--- backend/src/processor/subscriber.ts
+++ backend/src/processor/subscriber.ts
@@ -1,6 +1,13 @@
 import logger from '../shared/logger';
+import redisClient from '../shared/redis';
 
 export async function startSubscriber() {
   logger.info('Processor subscriber started ');
- 
+  try {
+    if (!redisClient.isOpen) {
+      await redisClient.connect();
+    }
+  } catch (err) {
+    throw err;
+  } 
 }
