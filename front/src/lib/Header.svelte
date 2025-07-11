d68f48398f0bd558afe7bb64b43ec03351ba9eaf
Martial <floquet.martial@gmail.com>
2025-07-11 15:58:08 +0200

    style(ui): change style for header

--- front/src/lib/Header.svelte
+++ front/src/lib/Header.svelte
@@ -1,3 +1,14 @@
-<header class="bg-green-100 py-6 mb-8 shadow">
-  <h1 class="text-3xl font-bold text-center text-gray-800">Calculator App</h1>
+<header class="py-6 mb-8  flex items-center">
+  <div class="w-13 h-13 form rounded-full ml-8"></div>
+
+  <h1 class=" p-5 text-3xl font-bold text-left text-gray-800 ml-5">Calculator App</h1>
 </header> 
+
+<style>
+  header {
+    background-color: #D8DDBB;
+  }
+  .form{
+    background-color: #b7bba0;
+  }
+</style>
