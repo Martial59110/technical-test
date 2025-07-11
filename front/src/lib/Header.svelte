cae58fc693177c158fa425367e2ebd594cdf76a7
Martial <floquet.martial@gmail.com>
2025-07-11 15:43:10 +0200

    feat(ui): add Header component with style

---
+++ front/src/lib/Header.svelte
@@ -0,0 +1,3 @@
+<header class="bg-green-100 py-6 mb-8 shadow">
+  <h1 class="text-3xl font-bold text-center text-gray-800">Calculator App</h1>
+</header> 
--- front/src/routes/+page.svelte
+++ front/src/routes/+page.svelte
@@ -4,12 +4,11 @@
   import CalculationHistory from '../lib/CalculationHistory.svelte';
 </script>
 
-<header>
-  <h1>Calculator App</h1>
+<header class="bg-green-100 py-6 mb-8 shadow">
+  <h1 class="text-3xl font-bold text-center text-gray-800">Calculator App</h1>
 </header>
 
 <main>
- 
   <CalculatorForm />
   <CalculationResult />
   <CalculationHistory />
