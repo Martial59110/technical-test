3d3c458596bd1cdda3f0c2a090798e39ea9e0c5f
Martial <floquet.martial@gmail.com>
2025-07-11 15:33:02 +0200

    feat(ui): add static CalculationHistory component

---
+++ front/src/lib/CalculationHistory.svelte
@@ -0,0 +1,11 @@
+<section>
+  <h2>History of Calculations</h2>
+  <ul>
+    <li>
+      <strong>Previous Calculation 1:</strong> 5 + 3 = <span>8</span>
+    </li>
+    <li>
+      <strong>Previous Calculation 2:</strong> 10 - 4 = <span>6</span>
+    </li>
+  </ul>
+</section> 
