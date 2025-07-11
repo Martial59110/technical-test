506a472eb1b6849795f0f31f2ab647226eb203ee
Martial <floquet.martial@gmail.com>
2025-07-11 12:31:04 +0200

    feat(processor): add operations for calculator service
    Add subtraction, multiplication and division

--- backend/src/processor/services/calculator.ts
+++ backend/src/processor/services/calculator.ts
@@ -1,13 +1,18 @@
 import { Operands } from '../../shared/types';
 
 export const operations: Operands = {
-  addition: (operand1: number, operand2: number): number => operand1 + operand2
+  addition: (operand1: number, operand2: number): number => operand1 + operand2,
+  subtraction: (operand1: number, operand2: number): number => operand1 - operand2,
+  multiplication: (operand1: number, operand2: number): number => operand1 * operand2,
+  division: (operand1: number, operand2: number): number => operand1 / operand2
 };
 
 
 export function performCalculation(operation: string, operands: number[]): number {
   const operationFunction = operations[operation];
-  
+  if (!operationFunction) {
+    throw new Error(`Unsupported operation: ${operation}`);
+  }
   
   return operationFunction(operands[0], operands[1]);
 }
