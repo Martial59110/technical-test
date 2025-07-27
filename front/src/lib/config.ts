5c923a43fd2c57a18f6d8a9bab94d9ab060a8e00
Martial <floquet.martial@gmail.com>
2025-07-27 23:55:43 +0200

    chore(front): format code with Prettier

--- front/src/lib/config.ts
+++ front/src/lib/config.ts
@@ -5,4 +5,4 @@ export const API_BASE_URL = `http://localhost:${env.PUBLIC_COLLECTOR_PORT}`;
 export const API_ENDPOINTS = {
 	COMPUTE: `${API_BASE_URL}/compute`,
 	TASKS: `${API_BASE_URL}/tasks`
-} as const; 
+} as const;
